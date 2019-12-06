526769f451d0d5a3eeb48cd03c6d7ecf254f5489
samuelkerosi <samuelbosire60@gmail.com>
2019-12-05 18:32:49 -0600

    More dashborad changes
    - sign up diableds for now
    - side bar works
    - routing
    - post and get work

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-// import { createBrowserHistory } from "history";
-import { mainRoutes } from "./routes";
-// import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import routes from "./routes";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 // core components
-// import Admin from "layouts/Admin.js";
+import Admin from "layouts/Admin.js";
 import "assets/css/material-dashboard-react.css?v=1.8.0";
+import SignIn from "views/SignIn/SignIn";
+import Dashboard from "views/Dashboard/Dashboard";
 
-// const hist = createBrowserHistory();
-const routes = mainRoutes();
+const hist = createBrowserHistory();
+// const routes = mainRoutes();
 
-ReactDOM.render(
-  // <Router history={hist}>
-  //   <Switch>
-  //     <Route path="/admin" component={Admin} />
-  //     <Redirect from="/" to="/admin/dashboard" />
-  //   </Switch>
-  // </Router>,
-  routes,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <Router history={hist}>
+//     <Switch>
+//       <Route path="/admin" component={Admin} />
+//       <Redirect from="/" to="/admin/dashboard" />
+//     </Switch>
+//   </Router>,
+//   routes,
+//   document.getElementById("root")
+// );
+
+class Index extends Component {
+  render(){
+    return(
+      <div>
+        <Router history={hist}>
+   <Switch>
+      <Route path="/admin" component={Admin} />
+       <Redirect from="/" to="dashboard" />
+     </Switch>
+   </Router>,
+    </div>
+    )
+  }
+}
+ReactDOM.render(<Index />, document.getElementById("root"))
--- src/routes.js
+++ src/routes.js
@@ -1,83 +1,125 @@
-// import Dashboard from "@material-ui/icons/Dashboard";
-// import Settings from "@material-ui/icons/Settings";
-// import LibraryBooks from "@material-ui/icons/LibraryBooks";
-// import BubbleChart from "@material-ui/icons/BubbleChart";
-// import LocationOn from "@material-ui/icons/LocationOn";
-// import Create from "@material-ui/icons/Create";
-// import Description from "@material-ui/icons/Description";
+import Dashboard from "@material-ui/icons/Dashboard";
+import Settings from "@material-ui/icons/Settings";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import BubbleChart from "@material-ui/icons/BubbleChart";
+import LocationOn from "@material-ui/icons/LocationOn";
+import Create from "@material-ui/icons/Create";
+import Description from "@material-ui/icons/Description";
 
 // // core components/views for Admin layout
-// import DashboardPage from "views/Dashboard/Dashboard.js";
-// import UserProfile from "views/UserProfile/UserProfile.js";
-// import TableList from "views/TableList/TableList.js";
-// import Summary from "views/Summary/Summary.js";
+import DashboardPage from "views/Dashboard/Dashboard.js";
+import UserProfile from "views/UserProfile/UserProfile.js";
+import TableList from "views/TableList/TableList.js";
+import Summary from "views/Summary/Summary.js";
 
-// import Icons from "views/Icons/Icons.js";
+import Icons from "views/Icons/Icons.js";
 
-// import NotificationsPage from "views/Notifications/Notifications.js";
-// // core components/views for RTL layout
+import NotificationsPage from "views/Notifications/Notifications.js";
+import PostCompany from "views/PostCompany/PostCompany";
+// core components/views for RTL layout
 
-// const dashboardRoutes = [
-//   {
-//     path: "/dashboard",
-//     name: "Dashboard",
-//     icon: Dashboard,
-//     component: DashboardPage,
-//     layout: "/admin"
-//   },
-//   {
-//     path: "/table",
-//     name: "Reports",
-//     icon: "content_paste",
-//     component: TableList,
-//     layout: "/admin"
-//   },
-//   {
-//     path: "/user",
-//     name: "Boundaries",
-//     icon: Create,
-//     component: UserProfile,
-//     layout: "/admin"
-//   },
-//   {
-//     path: "/summary",
-//     name: "Settings",
-//     icon: Settings,
-//     component: Summary,
-//     layout: "/admin"
-//   }
-// ];
-
-// export default dashboardRoutes;
-
-import React from "react";
-import { Route, Router } from "react-router-dom";
-import App from "./App";
-import Callback from "./Callback/Callback";
-import Auth from "./Auth/Auth";
-import history from "./history";
-
-const auth = new Auth();
-
-const handleAuthentication = ({ location }) => {
-  if (/access_token|id_token|error/.test(location.hash)) {
-    auth.handleAuthentication();
+const mainRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin"
+  },
+  {
+    path: "/table",
+    name: "Reports",
+    icon: "content_paste",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/user",
+    name: "Boundaries",
+    icon: Create,
+    component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/postcompany",
+    name: "Company Settings",
+    icon: Create,
+    component: PostCompany,
+    layout: "/admin"
+  },
+  {
+    path: "/summary",
+    name: "Settings",
+    icon: Settings,
+    component: Summary,
+    layout: "/admin"
   }
-};
+];
 
-export const mainRoutes = () => {
-  return (
-    <Router history={history}>
-      <div>
-        <Route path="/" render={props => <App auth={auth} {...props} />} />
-        <Route
-          path="/callback"
-          render={props => {
-            handleAuthentication(props);
-            return <Callback {...props} />;
-          }}
-        />
-      </div>
-    </Router>
-  );
-};
+export default mainRoutes;
+
+// import React from "react";
+// import { Route, Router } from "react-router-dom";
+// import App from "./App";
+// import Callback from "./Callback/Callback";
+// import Auth from "./Auth/Auth";
+// import history from "./history";
+
+// const auth = new Auth();
+
+// const handleAuthentication = ({ location }) => {
+//   if (/access_token|id_token|error/.test(location.hash)) {
+//     auth.handleAuthentication();
+//   }
+// };
+
+// export const mainRoutes = () => {
+//   return (
+//     <Router history={history}>
+//       <div>
+//         <Route path="/" render={props => <App auth={auth} {...props} />} />
+//         <Route
+//           path="/callback"
+//           render={props => {
+//             handleAuthentication(props);
+//             return <Callback {...props} />;
+//           }}
+//         />
+//       </div>
+//     </Router>
+//   );
+// };
+
+
+
+
+//  export const mainRoutes = () => [
+//     {
+//       path: "/dashboard",
+//       name: "Dashboard",
+//       icon: Dashboard,
+//       component: DashboardPage,
+//       layout: "/admin"
+//     },
+//     {
+//       path: "/table",
+//       name: "Reports",
+//       icon: "content_paste",
+//       component: TableList,
+//       layout: "/admin"
+//     },
+//     {
+//       path: "/user",
+//       name: "Boundaries",
+//       icon: Create,
+//       component: UserProfile,
+//       layout: "/admin"
+//     },
+//     {
+//       path: "/summary",
+//       name: "Settings",
+//       icon: Settings,
+//       component: Summary,
+//       layout: "/admin"
+//     }
+//   ];
